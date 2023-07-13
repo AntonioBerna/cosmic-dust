1fa04a8f3e847bcbdaf3a6ad2d64c6d42a4ac387
AntonioBerna <asukeasukai12@gmail.com>
2023-07-13 23:59:38 +0200

    fix js :zap:

--- js/index.js
+++ js/index.js
@@ -71,100 +71,199 @@ TweenMax.staggerFrom(".heading", 0.8, { opacity: 0, y: 20, delay: 0.2 }, 0.4);
 */
 
 
-window.AudioContext = window.AudioContext || window.webkitAudioContext || window.mozAudioContext;
+// window.AudioContext = window.AudioContext || window.webkitAudioContext || window.mozAudioContext;
 
-document.querySelector('audio').addEventListener('playing', function () {
-    var audio = document.getElementById('audio');
-    audio.onended = function () {
-        isPlaying = false;
-    };
+// document.querySelector('audio').addEventListener('playing', function () {
+//     var audio = document.getElementById('audio');
+//     audio.onended = function () {
+//         isPlaying = false;
+//     };
+//     var ctx = new AudioContext();
+//     var analyser = ctx.createAnalyser();
+//     var audioSrc = ctx.createMediaElementSource(audio);
+//     // we have to connect the MediaElementSource with the analyser 
+//     audioSrc.connect(analyser);
+//     analyser.connect(ctx.destination);
+//     // we could configure the analyser: e.g. analyser.fftSize (for further infos read the spec)
+//     // analyser.fftSize = 64;
+//     // frequencyBinCount tells you how many values you'll receive from the analyser
+//     var frequencyData = new Uint8Array(analyser.frequencyBinCount);
+
+//     // we're ready to receive some data!
+//     var canvas = document.getElementById('canvas'),
+//         cwidth = canvas.width,
+//         cheight = canvas.height - 2,
+//         meterWidth = 10, //width of the meters in the spectrum
+//         gap = 2, //gap between meters
+//         capHeight = 2,
+//         capStyle = '#fff',
+//         meterNum = 800 / (10 + 2), //count of the meters
+//         capYPositionArray = []; ////store the vertical position of hte caps for the preivous frame
+//     ctx = canvas.getContext('2d'),
+//         gradient = ctx.createLinearGradient(0, 0, 0, 300);
+//     gradient.addColorStop(1, '#e55d87');
+//     gradient.addColorStop(0.5, '#5fc3e4');
+//     // loop
+//     function renderFrame() {
+//         var array = new Uint8Array(analyser.frequencyBinCount);
+//         analyser.getByteFrequencyData(array);
+//         var step = Math.round(array.length / meterNum); //sample limited data from the total array
+//         ctx.clearRect(0, 0, cwidth, cheight);
+//         for (var i = 0; i < meterNum; i++) {
+//             var value = array[i * step];
+//             if (capYPositionArray.length < Math.round(meterNum)) {
+//                 capYPositionArray.push(value);
+//             };
+//             ctx.fillStyle = capStyle;
+//             //draw the cap, with transition effect
+//             if (value < capYPositionArray[i]) {
+//                 ctx.fillRect(i * 12, cheight - (--capYPositionArray[i]), meterWidth, capHeight);
+//             } else {
+//                 ctx.fillRect(i * 12, cheight - value, meterWidth, capHeight);
+//                 capYPositionArray[i] = value;
+//             };
+//             ctx.fillStyle = gradient; //set the filllStyle to gradient for a better look
+//             ctx.fillRect(i * 12 /*meterWidth+gap*/, cheight - value + capHeight, meterWidth, cheight); //the meter
+//         }
+//         requestAnimationFrame(renderFrame);
+//     }
+//     renderFrame();
+//     audio.play();
+// });
+
+// var isPlaying = false;
+// var currentSourceId = null;
+
+// function playSource(sourceId) {
+//     var audioPlayer = document.getElementById("audio");
+//     var source = document.getElementById("source" + sourceId);
+
+//     if (sourceId !== currentSourceId) {
+//         // Se è stato premuto un bottone diverso da quello corrente
+//         if (isPlaying) {
+//             // Se una canzone è già in riproduzione, interrompi la riproduzione
+//             audioPlayer.pause();
+//             isPlaying = false;
+//         }
+
+//         // Modifica l'attributo 'src' dell'elemento audio
+//         audioPlayer.src = source.src;
+
+//         // Avvia la riproduzione
+//         audioPlayer.play();
+//         isPlaying = true;
+//         currentSourceId = sourceId;
+//     } else {
+//         // Se è stato premuto lo stesso bottone
+//         if (isPlaying) {
+//             // Interrompi la riproduzione
+//             audioPlayer.pause();
+//             isPlaying = false;
+//         } else {
+//             // Avvia la riproduzione
+//             audioPlayer.play();
+//             isPlaying = true;
+//         }
+//     }
+// }
+
+
+var AudioContext = window.AudioContext || window.webkitAudioContext || window.mozAudioContext;
+if (AudioContext) {
+    var audioPlayer = document.getElementById("audio");
+    var canvas = document.getElementById("canvas");
+    var isPlaying = false;
+    var currentSourceId = null;
+
     var ctx = new AudioContext();
     var analyser = ctx.createAnalyser();
-    var audioSrc = ctx.createMediaElementSource(audio);
-    // we have to connect the MediaElementSource with the analyser 
+    var audioSrc = ctx.createMediaElementSource(audioPlayer);
     audioSrc.connect(analyser);
     analyser.connect(ctx.destination);
-    // we could configure the analyser: e.g. analyser.fftSize (for further infos read the spec)
-    // analyser.fftSize = 64;
-    // frequencyBinCount tells you how many values you'll receive from the analyser
-    var frequencyData = new Uint8Array(analyser.frequencyBinCount);
 
-    // we're ready to receive some data!
-    var canvas = document.getElementById('canvas'),
-        cwidth = canvas.width,
-        cheight = canvas.height - 2,
-        meterWidth = 10, //width of the meters in the spectrum
-        gap = 2, //gap between meters
-        capHeight = 2,
-        capStyle = '#fff',
-        meterNum = 800 / (10 + 2), //count of the meters
-        capYPositionArray = []; ////store the vertical position of hte caps for the preivous frame
-    ctx = canvas.getContext('2d'),
-        gradient = ctx.createLinearGradient(0, 0, 0, 300);
+    var cwidth = canvas.width;
+    var cheight = canvas.height - 2;
+    var meterWidth = 10;
+    var gap = 2;
+    var capHeight = 2;
+    var capStyle = '#fff';
+    var meterNum = 800 / (10 + 2);
+    var capYPositionArray = [];
+    var ctx = canvas.getContext('2d');
+    var gradient = ctx.createLinearGradient(0, 0, 0, 300);
     gradient.addColorStop(1, '#e55d87');
     gradient.addColorStop(0.5, '#5fc3e4');
-    // loop
+
     function renderFrame() {
         var array = new Uint8Array(analyser.frequencyBinCount);
         analyser.getByteFrequencyData(array);
-        var step = Math.round(array.length / meterNum); //sample limited data from the total array
+        var step = Math.round(array.length / meterNum);
         ctx.clearRect(0, 0, cwidth, cheight);
         for (var i = 0; i < meterNum; i++) {
             var value = array[i * step];
             if (capYPositionArray.length < Math.round(meterNum)) {
                 capYPositionArray.push(value);
-            };
+            }
             ctx.fillStyle = capStyle;
-            //draw the cap, with transition effect
             if (value < capYPositionArray[i]) {
                 ctx.fillRect(i * 12, cheight - (--capYPositionArray[i]), meterWidth, capHeight);
             } else {
                 ctx.fillRect(i * 12, cheight - value, meterWidth, capHeight);
                 capYPositionArray[i] = value;
-            };
-            ctx.fillStyle = gradient; //set the filllStyle to gradient for a better look
-            ctx.fillRect(i * 12 /*meterWidth+gap*/, cheight - value + capHeight, meterWidth, cheight); //the meter
+            }
+            ctx.fillStyle = gradient;
+            ctx.fillRect(i * 12, cheight - value + capHeight, meterWidth, cheight);
         }
         requestAnimationFrame(renderFrame);
     }
+
     renderFrame();
-    audio.play();
-});
 
-var isPlaying = false;
-var currentSourceId = null;
+    audioPlayer.addEventListener('playing', function () {
+        audioPlayer.onended = function () {
+            isPlaying = false;
+        };
 
-function playSource(sourceId) {
-    var audioPlayer = document.getElementById("audio");
-    var source = document.getElementById("source" + sourceId);
-
-    if (sourceId !== currentSourceId) {
-        // Se è stato premuto un bottone diverso da quello corrente
-        if (isPlaying) {
-            // Se una canzone è già in riproduzione, interrompi la riproduzione
+        if (currentSourceId !== null && currentSourceId !== audioPlayer.currentSrc) {
             audioPlayer.pause();
             isPlaying = false;
         }
 
-        // Modifica l'attributo 'src' dell'elemento audio
-        audioPlayer.src = source.src;
+        currentSourceId = audioPlayer.currentSrc;
 
-        // Avvia la riproduzione
-        audioPlayer.play();
-        isPlaying = true;
-        currentSourceId = sourceId;
-    } else {
-        // Se è stato premuto lo stesso bottone
-        if (isPlaying) {
-            // Interrompi la riproduzione
-            audioPlayer.pause();
-            isPlaying = false;
-        } else {
-            // Avvia la riproduzione
+        if (!isPlaying) {
             audioPlayer.play();
             isPlaying = true;
         }
+    });
+
+    function playSource(sourceId) {
+        var source = document.getElementById("source" + sourceId);
+        audioPlayer.src = source.src;
+
+        if (currentSourceId !== source.src) {
+            if (isPlaying) {
+                audioPlayer.pause();
+                isPlaying = false;
+            }
+
+            audioPlayer.play();
+            isPlaying = true;
+            currentSourceId = source.src;
+        } else {
+            if (isPlaying) {
+                audioPlayer.pause();
+                isPlaying = false;
+            } else {
+                audioPlayer.play();
+                isPlaying = true;
+            }
+        }
     }
+} else {
+    // Il browser non supporta l'API AudioContext
+    // Gestisci il caso in cui l'audio non sarà disponibile
+    console.log("Il browser non supporta l'API AudioContext.");
 }
 
 
