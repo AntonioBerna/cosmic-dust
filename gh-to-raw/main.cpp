852979fef2fa6797230e7e81fa72b4a2314ca5e5
AntonioBerna <asukeasukai12@gmail.com>
2023-07-19 16:13:53 +0200

    fixed bug style of code :rocket:

--- gh-to-raw/main.cpp
+++ gh-to-raw/main.cpp
@@ -1,9 +1,6 @@
 #include <iostream>
 #include <string>
 
-#define SEARCH "github.com"
-#define REPLACE "raw.githubusercontent.com"
-
 std::string get_raw(const std::string &url, const std::string &search, const std::string &replace) {
 	std::string raw_url = url;
 	size_t pos = 0;
@@ -16,15 +13,25 @@ std::string get_raw(const std::string &url, const std::string &search, const std
 	return raw_url;
 }
 
-int main(int argc, const char **argv) {	
+int main(int argc, const char **argv) {
+	const std::string searches[] = {
+		"github.com",
+		"/blob"
+	};
+
+	const std::string replaces[] = {
+		"raw.githubusercontent.com",
+		""
+	};
+
 	if (argc != 2) {
 		std::cout << "Usage: " << argv[0] << " <url>" << std::endl;
 		return 1;
 	}
 	
 	std::string url = *(argv + 1);
-	std::string raw_url = get_raw(url, SEARCH, REPLACE);
-	raw_url = get_raw(raw_url, "/blob", "");
+	std::string raw_url = get_raw(url, searches[0], replaces[0]);
+	raw_url = get_raw(raw_url, searches[1], replaces[1]);
 	std::cout << "Converted URL: " << raw_url << std::endl;
 	
 	return 0;
