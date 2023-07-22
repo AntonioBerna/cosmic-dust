1a55819cd2f8425543a73e9b395cdee96d894260
AntonioBerna <asukeasukai12@gmail.com>
2023-07-22 02:12:05 +0200

    improve structure of the code :art:

--- gh-to-raw/main.cpp
+++ gh-to-raw/main.cpp
@@ -1,19 +1,29 @@
 #include <iostream>
 #include <string>
 
-std::string get_raw(const std::string &url, const std::string &search, const std::string &replace) {
+std::string get_raw(const std::string &url, const std::string *searches, const std::string *replaces, size_t num_searches) {
 	std::string raw_url = url;
-	size_t pos = 0;
 
-	while ((pos = raw_url.find(search, pos)) != std::string::npos) {
-		raw_url.replace(pos, search.length(), replace);
-		pos += replace.length();
+	for (size_t i = 0; i < num_searches; ++i) {
+		size_t pos = 0;
+		const std::string &search = searches[i];
+		const std::string &replace = replaces[i];
+
+		while ((pos = raw_url.find(search, pos)) != std::string::npos) {
+			raw_url.replace(pos, search.length(), replace);
+			pos += replace.length();
+		}
 	}
 
 	return raw_url;
 }
 
 int main(int argc, const char **argv) {
+	if (argc != 2) {
+		std::cout << "Usage: " << *argv << " <url>" << std::endl;
+		return 1;
+	}
+
 	const std::string searches[] = {
 		"github.com",
 		"/blob"
@@ -23,17 +33,12 @@ int main(int argc, const char **argv) {
 		"raw.githubusercontent.com",
 		""
 	};
-
-	if (argc != 2) {
-		std::cout << "Usage: " << argv[0] << " <url>" << std::endl;
-		return 1;
-	}
 	
 	std::string url = *(argv + 1);
-	std::string raw_url = get_raw(url, searches[0], replaces[0]);
-	raw_url = get_raw(raw_url, searches[1], replaces[1]);
+	size_t num_searches = sizeof(searches) / sizeof(searches[0]);
+	
+	std::string raw_url = get_raw(url, searches, replaces, num_searches);
 	std::cout << "Converted URL: " << raw_url << std::endl;
 	
 	return 0;
 }
-
